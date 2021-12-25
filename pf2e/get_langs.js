3132a8f19a3c21a068178dc0c042bd7d5dc1477d
Redpisces <redpisces@live.com>
2021-12-25 10:02:16 -0500

    Add files via upload

---
+++ pf2e/get_langs.js
@@ -0,0 +1,47 @@
+const SPEAK_PRIVATE = true;
+const IGNORE_GMPC = true;
+const ACTOR_TYPES = ["character"];
+
+function makeChatMessage(content){
+	let chatMessageData = {
+		type:CONST.CHAT_MESSAGE_TYPES.OOC,
+		content:content,
+		user:game.user._id,
+		speaker: ChatMessage.getSpeaker(),
+	};
+	if (SPEAK_PRIVATE){chatMessageData.whisper=[game.user.id]};
+	let c = ChatMessage.create(chatMessageData);
+};
+
+function getPlayerCharacters(){
+	let pcs = game.actors.filter(pc=>ACTOR_TYPES.includes(pc.data.type));
+	
+	if (IGNORE_GMPC){
+		const players = game.users.filter(player=>player.role!=CONST.USER_ROLES.GAMEMASTER);
+
+		let playerIDs=new Array();
+
+		players.forEach(pc=>playerIDs.push(pc.id));
+
+		return pcs.filter(pc=> Object.keys(pc.data.permission).some(r=>playerIDs.includes(r)));
+	} else {
+		return pcs
+	}
+}
+
+const playerCharacters = getPlayerCharacters();
+
+let returnString="<table><tr><th>character</th><th>languages</th></tr>";
+
+playerCharacters.forEach(pc=>{
+	returnString+="<tr><td>"+pc.name+"</td><td>";
+	pc.data.data.traits.languages.value.forEach(
+		function(lang,idx,array){
+			returnString+=lang;
+			if (idx != array.length -1){returnString+=", "};
+		}
+	);
+	returnString+="</td></tr>";
+});
+returnString+="</table>";
+makeChatMessage(returnString);
