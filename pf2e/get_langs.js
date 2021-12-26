0fa91a4d16ce1dad0495f172d793c5f4354f705d
Redpisces <redpisces@live.com>
2021-12-25 19:08:05 -0500

    added filter by active users

--- pf2e/get_langs.js
+++ pf2e/get_langs.js
@@ -1,5 +1,7 @@
 const SPEAK_PRIVATE = true;
 const PLAYER_OWNED_ONLY = true;
+const ACTIVE_ONLY = false;
+
 const ACTOR_TYPES = ["character"];
 const IGNORED_CLASSES = ["Animal Companion"];
 
@@ -25,33 +27,62 @@ function makeChatMessage(content) {
 };
 
 function getPlayerCharacters() {
-	let characters = game.actors.filter(pc => ACTOR_TYPES.includes(pc.data.type));
 
-	//FILTER OUT UNDESIRABLE CLASSES
+	//filter characters
+	let characters = game.actors;
 
-	characters = characters.filter(char => !isForbiddenClass(char));
-
-	if (PLAYER_OWNED_ONLY) {
-		return characters.filter(pc => pc.hasPlayerOwner);
-	} else {
-		return characters
+	if (ACTOR_TYPES) {
+		characters = characters.filter(pc => ACTOR_TYPES.includes(pc.data.type));
 	}
+
+	if (IGNORED_CLASSES) {
+		characters = characters.filter(char => !isForbiddenClass(char));
+	}
+
+	//filter users
+	let validUsers = game.users;
+
+	if (ACTIVE_ONLY) {
+		validUsers = validUsers.filter(u => u.active);
+	}
+	if (PLAYER_OWNED_ONLY) {
+		validUsers = validUsers.filter(u => u.role < CONST.USER_ROLES.GAMEMASTER);
+	}
+
+	let validUserIds = [];
+	validUsers.forEach(u => validUserIds.push(u.data._id));
+
+	//filter characters by users
+	characters = characters.filter(function(char) {
+		let owners = Object.keys(char.data.permission);
+		for (let id of owners) {
+			if (validUserIds.includes(id))
+				return true;
+		}
+		return false;
+	});
+
+	return characters;
+
 }
 
-
+//construct chat message content
 let returnString = "<table><tr><th>character</th><th>languages</th></tr>";
 
-getPlayerCharacters().forEach(pc => {
-	returnString += "<tr><td>" + pc.name + "</td><td>";
-	pc.data.data.traits.languages.value.forEach(
-		function(lang, idx, array) {
-			returnString += lang;
-			if (idx != array.length - 1) {
-				returnString += ", "
-			};
-		}
-	);
-	returnString += "</td></tr>";
-});
+getPlayerCharacters()
+	.forEach(pc => {
+		returnString += "<tr><td>" + pc.name + "</td><td>";
+		pc.data.data.traits.languages.value.forEach(
+			function(lang, idx, array) {
+				returnString += lang;
+				if (idx != array.length - 1) {
+					returnString += ", "
+				};
+			}
+		);
+		returnString += "</td></tr>";
+	});
 returnString += "</table>";
+
+//output
 makeChatMessage(returnString);
