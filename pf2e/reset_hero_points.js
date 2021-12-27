e871bbf38486fed25b935f947eb8c5f9e90bcd7b
Redpisces <redpisces@live.com>
2021-12-26 21:36:03 -0500

    added confirm dialog

--- pf2e/reset_hero_points.js
+++ pf2e/reset_hero_points.js
@@ -1,5 +1,10 @@
 const DISPLAY_CHAT_MESSAGE = true;
 
+function logWithNotify(message) {
+	console.log(message);
+	ui.notifications.notify(message);
+}
+
 function getChars() {
 	let returnedActors = []
 	game.actors.forEach(a => {
@@ -10,6 +15,14 @@ function getChars() {
 	return returnedActors;
 }
 
+let confirmation = await Dialog.confirm({
+	title: 'Reset all hero points',
+	content: `<p>this will reset ALL PCs' hero points to 1 </p><p>Are you sure?</p>`,
+});
+
+if (!confirmation)
+	return;
+
 const update = {
 	'data.resources.heroPoints.value': 1
 }
@@ -17,5 +30,4 @@ let returnedActors = getChars();
 for (actor of returnedActors) {
 	let updated = await actor.update(update)
 }
-
-ui.notifications.notify(`reset ${returnedActors.length} characters' hero points`);
+logWithNotify(`reset ${returnedActors.length} characters' hero points`);
